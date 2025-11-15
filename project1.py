755cf1fb53691c22d320e6073d2e59de74c34864
D-hue-cloud <dinugaratnayake@gmail.com>
2025-11-15 20:24:02 +0000

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,13 @@
+#Taking in a number from user
+num=int(input("We can √ Any Number (no commas):\n"))
+print("Lets do it!")
+#Square rooting a given number
+sqrt=num**0.5
+print(sqrt)
+print("Thank you for using this!!")
+#Encouraging user to try again
+print("\n\nYou can try our squaring machine too!")
+num2=int(input("Lets square a number!\n"))
+print("Cogs are turning!")
+print(num2**2)
+print("Goodbye!!!!!!!!!!!")
